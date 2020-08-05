6a2b45087e545daef8ff2095331906db13bb4f89
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-08-05 19:05:59 -0400

    Update index.js

--- react-sorting-hat/src/index.js
+++ react-sorting-hat/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BgStyle = {
+    backgroundImage: `url("/static/media/housecrests.7925ec0b.png")`,
+    backgroundRepeat: `no-repeat`,
+    backgroundSize: `50% 50%`,
+    backgroundPosition: `center`
+  }
+
+ReactDOM.render(<div style={BgStyle}><App /></div>, document.getElementById('root'));
